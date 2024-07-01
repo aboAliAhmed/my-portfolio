2da0e1d416258ded2c7e5a02a7b868e8b8cc4099
aboAliAhmed <ahmedbeasar@gmail.com>
2024-07-01 19:58:44 +0300

    edit social media colors

--- components/boi.tsx
+++ components/boi.tsx
@@ -34,28 +34,28 @@ export default function Boi() {
         <a 
           href={'https://github.com/aboAliAhmed'} 
           target='_blank' 
-          className="text-black dark:text-white hover:text-zinc-800 dark:hover:bg-black rounded-full"
+          className="text-zinc-500 dark:text-white hover:text-zinc-800 dark:hover:bg-black rounded-full"
         >
           <FaGithub className="w-10 h-10" />
         </a>
         <a
           href={'https://www.linkedin.com/in/ahmed-besar-944220220/'} 
           target='_blank' 
-          className="text-sky-500 dark:text-white hover:text-sky-600 dark:hover:text-sky-800"
+          className="text-zinc-500 dark:text-white hover:text-sky-600 dark:hover:text-sky-800"
         >
           <FaLinkedinIn className="w-9 h-9 p-[2px]" />
         </a>
         <a 
           href={'https://www.facebook.com/ahmedabderrahmanbesar/'} 
           target='_blank' 
-          className="text-blue-700 hover:text-blue-900/90 dark:text-white dark:hover:text-blue-800/90 "
+          className="text-zinc-500 hover:text-blue-700 dark:text-white dark:hover:text-blue-800/90 "
         >
           <FaFacebook className="w-10 h-10 rounded-full" />
         </a>
         <a
           href={'http://wa.me/+201274625521'} 
           target='_blank' 
-          className="bg-white text-green-500 hover:text-white hover:bg-green-500 dark:bg-transparent dark:text-white dark:hover:text-zinc-200 dark:hover:bg-[#0cc042] rounded-full rounded-bl-none" 
+          className="bg-white text-zinc-500 hover:text-white hover:bg-green-500 dark:bg-transparent dark:text-white dark:hover:text-zinc-200 dark:hover:bg-[#0cc042] rounded-full rounded-bl-none" 
         >
           <FaWhatsapp className="w-10 h-10" />
         </a>
